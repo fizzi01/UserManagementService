7f6dc9929b460bb3a9bbfb70725d27ab86d0d966
fizzi01 <federico.izzi@studenti.unisalento.it>
2024-05-21 00:21:51 +0200

    Security hot fix

--- src/main/java/it/unisalento/pasproject/usermanagementservice/security/JwtAuthenticationFilter.java
+++ src/main/java/it/unisalento/pasproject/usermanagementservice/security/JwtAuthenticationFilter.java
@@ -8,6 +8,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -26,28 +28,35 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Autowired
     private UserCheckService customerUserDetailsService ;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws ServletException, IOException, UserNotAuthorizedException, AccessDeniedException {
 
         final String authorizationHeader = request.getHeader("Authorization");
 
+        LOGGER.info("Authorization header: " + authorizationHeader);
+
         String username = null;
         String jwt = null;
 
         try {
+            LOGGER.info("Extracting username from JWT token");
             if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
                 jwt = authorizationHeader.substring(7);
                 username = jwtUtilities.extractUsername(jwt);
+            } else {
+                LOGGER.info("Missing token");
+                throw new AccessDeniedException("Missing token");
             }
         } catch (Exception e) {
-            chain.doFilter(request, response);
-            throw new AccessDeniedException("Invalid token");
+            throw new AccessDeniedException(e.getMessage());
         }
 
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-
+            LOGGER.info("Checking user details");
             User user = this.customerUserDetailsService.loadUserByUsername(username);
             UserDetails userDetails = org.springframework.security.core.userdetails.User.builder()
                     .username(user.getEmail()) // Assume email is username
@@ -62,10 +71,14 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                         .setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                 SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
             } else {
-                chain.doFilter(request, response);
                 throw new UserNotAuthorizedException("User not authorized");
             }
         }
+
+        if ( SecurityContextHolder.getContext().getAuthentication() == null ) {
+            throw new AccessDeniedException("No authentication found");
+        }
+
         chain.doFilter(request, response);
     }
 
